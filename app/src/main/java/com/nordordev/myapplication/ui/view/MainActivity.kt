1eea38e22a27edb687dc479399c76624bc814d5e
Sebastian Salgado <salgadoseba89@gmail.com>
2023-01-21 23:13:27 -0300

    [ Fix: Move MainActivity.kt to the activity package  ]

--- app/src/main/java/com/nordordev/myapplication/ui/view/MainActivity.kt
+++ app/src/main/java/com/nordordev/myapplication/ui/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.nordordev.myapplication
+package com.nordordev.myapplication.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nordordev.myapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
